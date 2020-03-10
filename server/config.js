d2ad973fb752f425777b62e41d04ee4ccf738222
Styfan <styfand456@gmail.com>
2020-03-10 10:05:19 +0100

    to host

--- server/config.js
+++ server/config.js
@@ -1,9 +1,5 @@
-exports.PORT = process.env.PORT || 8080
+exports.PORT = 8080
 
-exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? process.env.CLIENT_ORIGIN
-  : 'http://localhost:3000'
+exports.CLIENT_ORIGIN = 'http://localhost:3000'
 
-exports.DB_URL = process.env.NODE_ENV === 'production'
-  ? process.env.DB_URL
-  : 'mongodb://localhost/simple-email-confirmation'
+exports.DB_URL = 'mongodb://mongo:27017/simple-email-confirmation'
