9845477763b51ad73df36defd00f833d6c0ab5cd
Styfan <styfand456@gmail.com>
2020-03-12 10:02:31 +0100

    e

--- server/config.js
+++ server/config.js
@@ -2,4 +2,4 @@ exports.PORT = 8080
 
 exports.CLIENT_ORIGIN = 'https://www.tracepic-backend.trimakus.com' && 'https://tracepic-backend.trimakus.com'
 
-exports.DB_URL = 'mongodb://localhost:27017/simple-email-confirmation' // 'mongodb://mongo:27017/simple-email-confirmation' //
+exports.DB_URL = 'mongodb://mongo:27017/simple-email-confirmation' //
