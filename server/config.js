5dcf32077a8feb2cb79187a6ab7e22f1ca9bfea3
sofienedim <dimessisofiene@gmail.com>
2020-09-22 10:41:21 +0100

    auto push -2-

--- server/config.js
+++ server/config.js
@@ -1,22 +1,7 @@
 exports.PORT = 8090
 
-// exports.CLIENT_ORIGIN = 'https://www.tracepic-backend.trimakus.com';
 exports.CLIENT_ORIGIN = 'http://51.75.246.191'
+// exports.CLIENT_ORIGIN = 'http://localhost:3000'
 
-// exports.CLIENTS_ORIGIN = ['https://www.tracepic-backend.trimakus.com', 'https://tracepic-backend.trimakus.com'];
-exports.CLIENTS_ORIGIN = ['https://www.tracepic-backend.trimakus.com', 'https://tracepic-backend.trimakus.com', 'http://localhost:3000'];
 
 exports.DB_URL = 'mongodb://localhost:27017/simple-email-confirmation'
-// exports.DB_URL = 'mongodb://51.178.53.74:27017/simple-email-confirmation'
-
-// exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-//     ? 'https://www.tracepic-backend.trimakus.com'
-//     : 'http://localhost:3000'
-
-// exports.CLIENTS_ORIGIN = process.env.NODE_ENV === 'production'
-//     ? ['https://www.tracepic-backend.trimakus.com', 'https://tracepic-backend.trimakus.com']
-//     : ['https://www.tracepic-backend.trimakus.com', 'https://tracepic-backend.trimakus.com', 'http://localhost:3000']
-
-// exports.DB_URL = process.env.NODE_ENV === 'production'
-//     ? 'mongodb://mongo:27017/simple-email-confirmation'
-//     : 'mongodb://localhost:27017/simple-email-confirmation'
